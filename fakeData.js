de1087dac5c42c4866dcdc7b7383ab9a0cfbe1b6
Sushant Dahiya <sushant_dahiya@homedepot.com>
2020-11-23 16:17:10 -0600

    more improvements

--- fakeData.js
+++ fakeData.js
@@ -1,4 +1,11 @@
-const util = require('./util')
+const util = require('./lib/util')
+const readline = require("readline");
+
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 /**
  * Number of iterations to run.
@@ -29,3 +36,23 @@ process.argv.forEach(function (val, index, array) {
 
 let rowFormats = schema;
 util.generateFakeData(iterations, rowFormats);
+
+rl.question("Do you also want to insert these rows in bigquery? (choices: y/n)  ", function(doInsert) {
+  if(doInsert.toLowerCase() == 'n') {
+    rl.close()
+  }
+  else {
+    rl.question("Provide dataset and table names separated by a comma:  ", function(tableParams) {
+      let dataset = tableParams.split(',')[0].trim()
+      let table = tableParams.split(',')[1].trim()
+      util.insertData(tableParams.split(',')[0])
+      rl.close();
+    });
+  }
+
+});
+
+rl.on("close", function() {
+  console.log("\nAdios!");
+  process.exit(0);
+});
